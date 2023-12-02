77eeab9b51ea0eb3d12fc5ba2ba8ae6a22e3e8b4
Adson Luis <adson.vitorio@fatec.sp.gov.br>
2023-12-02 01:18:15 -0300

    Implementando filtros na lista de chamada

--- pages/rollcall.js
+++ pages/rollcall.js
@@ -3,31 +3,181 @@ import { useState, useEffect } from "react";
 import { getCookie, setCookie, deleteCookie } from "cookies-next";
 import nProgress from "nprogress";
 import Logo from "@/components/Logo";
-import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function RollCall() {
   const [presences, setPresences] = useState([]);
   const [Disabled, setDisabled] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [filteredPresences, setFilteredPresences] = useState([]);
+  const [selectedPeriod, setSelectedPeriod] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [renderedItems, setRenderedItems] = useState([]);
+
+  const fetchUserData = async (ra) => {
+    try {
+      const response = await fetch(
+        `https://projeto-sacha.onrender.com/users/single/ra?ra=${ra}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        return data.formattedData;
+      } else {
+        throw Error("Usuário não encontrado");
+      }
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  const renderEstudantItems = async () => {
+    const term = searchTerm.toLowerCase();
+
+    const renderedItems = await Promise.all(
+      filteredPresences.map(async (presence) => {
+        const presenceDate = new Date(presence.date_create);
+        const hours = presenceDate.getHours();
+        const minutes = presenceDate.getMinutes();
+
+        const userData = await fetchUserData(presence.estudant_RA);
+
+        if (!userData) {
+          return null;
+        }
+
+        const estudantName = userData.Nome.toLowerCase();
+
+        if (
+          (selectedPeriod === "Selecione um período" ||
+            (selectedPeriod === "1º Período" &&
+              (hours === 19 || (hours === 20 && minutes <= 40))) ||
+            (selectedPeriod === "2º Período" &&
+              hours === 20 &&
+              minutes >= 50) ||
+            (selectedPeriod === "2º Período" &&
+              hours === 22 &&
+              minutes <= 30)) &&
+          estudantName.includes(term)
+        ) {
+          return (
+            <EstudantItem
+              key={`${presence.estudant_RA}_${formatDate(
+                presence.date_create
+              )}`}
+              ra={presence.estudant_RA}
+              classe={presence.class}
+              userData={userData}
+            />
+          );
+        }
+
+        return null;
+      })
+    );
+
+    setRenderedItems(renderedItems.filter((item) => item !== null));
+  };
 
   useEffect(() => {
-    const fetchPresences = async () => {
-      try {
-        const response = await fetch(
-          "https://projeto-sacha.onrender.com/presences/all"
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setPresences(data.presences);
-        } else {
-          console.error("Failed to fetch presences");
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
+    renderEstudantItems();
+  }, [filteredPresences, selectedPeriod, searchTerm]);
 
+  const fetchPresences = async () => {
+    try {
+      const response = await fetch(
+        "https://projeto-sacha.onrender.com/presences/all"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setPresences(data.presences);
+        await renderEstudantItems();
+      } else {
+        console.error("Presença não encontrada");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
     fetchPresences();
   }, []);
+
+  useEffect(() => {
+    if (selectedDate) {
+      const filteredPresences = presences.filter((presence) => {
+        const presenceDate = new Date(presence.date_create);
+        return (
+          presenceDate.getDate() === selectedDate.getDate() &&
+          presenceDate.getMonth() === selectedDate.getMonth() &&
+          presenceDate.getFullYear() === selectedDate.getFullYear()
+        );
+      });
+
+      setFilteredPresences(filteredPresences);
+
+      const periodSelectElement = document.getElementById("periodSelect");
+
+      const selectedPeriodValue = periodSelectElement
+        ? periodSelectElement.value
+        : "Selecione um período";
+
+      const event = {
+        target: {
+          value: selectedPeriodValue,
+        },
+      };
+
+      handlePeriodChange(event);
+      renderEstudantItems();
+    } else {
+      setFilteredPresences(presences);
+
+      const periodSelectElement = document.getElementById("periodSelect");
+
+      const selectedPeriodValue = periodSelectElement
+        ? periodSelectElement.value
+        : "Selecione um período";
+
+      const event = {
+        target: {
+          value: selectedPeriodValue,
+        },
+      };
+
+      handlePeriodChange(event);
+      renderEstudantItems();
+    }
+  }, [selectedDate, presences, selectedPeriod, searchTerm]);
+
+  const handleDateChange = async (date) => {
+    setSelectedDate(date);
+    await renderEstudantItems();
+  };
+
+  const handlePeriodChange = async (event) => {
+    setSelectedPeriod(event.target.value);
+    await renderEstudantItems();
+  };
+
+  const handleInputChange = async (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+    await renderEstudantItems();
+  };
+
+  const formatDate = (inputDate) => {
+    const date = new Date(inputDate);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${day}-${month}-${year}-${hours}-${minutes}-${seconds}`;
+  };
 
   const handleClick = (event) => {
     nProgress.start();
@@ -81,15 +231,50 @@ function RollCall() {
     <div className="container py-4 text-center">
       <div className="container-fluid">
         <Logo />
-        <div className="d-flex flex-column mb-3">
-          <div id="list" className="list-group">
-            {presences.map((presence) => (
-              <EstudantItem
-                key={presence.estudant_RA}
-                ra={presence.estudant_RA}
-                classe={presence.class}
+        <div className="d-flex flex-column justify-content align-items-center mb-3">
+          <div className="d-flex flex-column flex-md-row">
+            <div className="mb-3 mb-md-0">
+              <DatePicker
+                className="form-select form-select-lg w-100"
+                placeholderText="Selecione uma data"
+                selected={selectedDate}
+                onChange={handleDateChange}
+                dateFormat="dd/MM/yyyy"
               />
-            ))}
+            </div>
+            <div className="mb-3 ms-md-3">
+              <select
+                id="periodSelect"
+                className="form-select form-select-lg w-100"
+                aria-label="Large select example"
+                onChange={handlePeriodChange}
+              >
+                <option defaultValue="0" selected>
+                  Selecione um período
+                </option>
+                <option defaultValue="1">1º Período</option>
+                <option defaultValue="2">2º Período</option>
+              </select>
+            </div>
+          </div>
+          <div className="input-group mb-3">
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control w-100"
+                id="floatingInputGroup1"
+                placeholder="Procurar um aluno"
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="floatingInputGroup1">Procurar um aluno</label>
+            </div>
+            <span className="input-group-text">
+              <i className="bi bi-search fw-bold red-text"></i>
+            </span>
+          </div>
+          <div id="list" className="list-group">
+            {renderedItems}
           </div>
           <div className="d-flex text-center justify-content-center align-items-center">
             {Disabled ? (
@@ -143,19 +328,19 @@ function EstudantItem({ ra, classe }) {
       className="list-group-item list-group-item-action bg"
       aria-current="true"
     >
-      <div className="d-flex align-items-center justify-content-center">
-        <Image
+      <div className="d-flex flex-column flex-md-row align-items-center justify-content-center">
+        <img
           src={userData.Foto}
-          className="rounded me-3"
+          className="rounded me-3 mb-3 mb-md-0"
           width={100}
           height={100}
           alt={"Foto do Estudante"}
         />
-        <div>
+        <div className="">
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">{userData.Nome}</h5>
           </div>
-          <div className="d-flex justify-content-evenly align-items-center">
+          <div className="d-flex flex-column flex-md-row justify-content-evenly align-items-center">
             <small>{classe ?? "Sem disciplina"}</small>
             <small>|</small>
             <small>{userData.Periodo}</small>
